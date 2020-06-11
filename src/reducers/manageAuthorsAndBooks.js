9b4a1ef281f982f134f5e815bb0432a2891fb822
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-11 02:10:39 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux"
 
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+})
+
 export default function bookApp(
   state = {
     authors: [],
